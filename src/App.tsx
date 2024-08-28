9c7be3c80ffab05974d718ebced457e59463d38e
luke <305867377@qq.com>
2024-08-28 23:15:28 +1000

    finished 007-10

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,29 @@ function App() {
 
   const [users, setUsers] = useState<User[]>([])
   const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     const controller = new AbortController();
 
     // get -> primise -> res / err
     axios.get<User[]>('https://jsonplaceholder.typicode.com/users', {signal: controller.signal}).then(
-      res => setUsers(res.data)
+      res => {
+        setUsers(res.data)
+        setIsLoading(false);
+      }
     ).catch(err => {
       if( err instanceof CanceledError) return
-      setError(err.message)})
+      setError(err.message)
+      setIsLoading(false);
+    })
+    
     
     return () => controller.abort()
   },[])
 
   return (
     <>
+        {isLoading && <div className="spinner-border"></div>}
        {error && <p className="text-danger">{error}</p>}
       <ul>
         {users.map( user => <li key={user.id}>{user.name}</li>)}
