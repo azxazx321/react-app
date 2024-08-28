e70d0a958b27215e539cb81f2a70bdcf9106408f
luke <305867377@qq.com>
2024-08-28 17:30:18 +1000

    finished 007-5 Axios Promise

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,32 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios, { Axios } from 'axios';
 
+interface User {
+  id: number;
+  name: string;
+
+}
 
 function App() {
   const connect = () => console.log('Connecting')
   const disconnect = () => console.log('Disconnecting')
 
+  const [users, setUsers] = useState<User[]>([])
 
   useEffect(() => {
-    connect();
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users').then(
+      res => setUsers(res.data)
+    )
+  },[])
 
-    return () => disconnect();
-  })
-
-  return (<div>
-      
+  return (
+      <ul>
+        {users.map( user => <li key={user.id}>{user.name}</li>)}
+      </ul>
    
-  </div>)
+  )
 }
 
 export default App;
