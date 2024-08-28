8da082d47025ef43f09584cbc2f3de412dd2394c
luke <305867377@qq.com>
2024-08-28 22:47:46 +1000

    finished 007-8 then.catch to try.catch

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios, { Axios } from 'axios';
+import axios, { Axios, AxiosError } from 'axios';
 
 interface User {
   id: number;
@@ -11,22 +11,35 @@ interface User {
 }
 
 function App() {
-  const connect = () => console.log('Connecting')
-  const disconnect = () => console.log('Disconnecting')
 
   const [users, setUsers] = useState<User[]>([])
-
+  const [error, setError] = useState('')
   useEffect(() => {
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users').then(
-      res => setUsers(res.data)
-    )
-  },[])
+    const fetchUsers = async () => {
+      // get -> primise -> res / err
+
+      try {
+        const res = await axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+        setUsers(res.data)
+      }
+      catch (err) {
+        setError((err as AxiosError).message)
+      }
+
+    }
+
+    fetchUsers();
+
+  }, [])
 
   return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
       <ul>
-        {users.map( user => <li key={user.id}>{user.name}</li>)}
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
-   
+    </>
+
   )
 }
 
