05fc4b087b5c1036bd4d6e45479f8051a160e989
luke <305867377@qq.com>
2024-08-28 17:19:43 +1000

    finished 007-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 
 import { useEffect, useRef, useState } from 'react';
 import './App.css'
-
-
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ProductList from './components/ProductList';
 
 
 function App() {
-  const [category, setCategory] = useState('')  
+  const connect = () => console.log('Connecting')
+  const disconnect = () => console.log('Disconnecting')
+
+
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  })
 
   return (<div>
-      <div>
-      <select className='form-select' onChange={(e) => setCategory(e.target.value)}>
-                <option value=""></option>
-                <option value="Clothing">Clothing</option>
-                <option value="Household">Household</option>
-        </select>
-        <ProductList category={category}/>
-      </div>
+      
    
   </div>)
 }
